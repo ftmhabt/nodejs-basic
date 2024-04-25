41467ccd3449368fbeabc515c37418f51d32dade
ftmhabt <ftmh.a78@gmail.com>
2024-04-25 17:53:48 +0330

    Add a basic app with routing

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    let path;
+    switch (req.url) {
+        case '/':
+            path = './index.html';
+            break;
+        case '/about':
+            path = './about.html';
+            break;
+        case '/contact-me':
+            path = './contact.html';
+            break;
+        default:
+            path = './404.html';
+            break;
+    }
+
+    fs.readFile(path,(err,data)=>{
+        if(err) throw err;
+        res.end(data);
+    })
+});
+
+server.listen(3000);
